6e7f04ec50f5dc2ba897f28851f0af259ca0fe96
gundorit <packdev937@gmail.com>
2023-05-31 20:46:26 +0900

    feat [login]: Implement session-based login logic
    - Create LoginDto.java
    - Create LoginService.java
    - Create LoginController.java
    - Create html based login logic
    - Update UserRepository.java

---
+++ src/main/java/com/market/carrot/controller/LoginController.java
@@ -0,0 +1,74 @@
+package com.market.carrot.controller;
+
+import com.market.carrot.dto.LoginDto;
+import com.market.carrot.entity.SessionConst;
+import com.market.carrot.entity.User;
+import com.market.carrot.service.LoginService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class LoginController {
+
+    private final LoginService loginService;
+
+    // 로그인 화면에서 이메일과 비밀번호를 입력 받는다
+    @GetMapping("/login")
+    public String loginForm(@ModelAttribute("loginForm") LoginDto userLoginDto) {
+        return "login/loginForm";
+    }
+
+    // 입력 받은 이메일과 비밀번호를 Post를 통해 받는다
+    @PostMapping("/login")
+    public String login(@Valid @ModelAttribute LoginDto loginDto, BindingResult bindingResult, HttpServletRequest request) {
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+
+        User loginUser = loginService.login(loginDto.getEmail(), loginDto.getPassword());
+        if (loginUser == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+        HttpSession session = request.getSession();
+        session.setAttribute(SessionConst.LOGIN_USER, loginUser);
+
+        return "redirect:/";
+    }
+
+    // Logout 요청이 들어오면 세션을 제거한다
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/")
+    public String homeLogin(@SessionAttribute(name = SessionConst.LOGIN_USER, required = false) User loginUser,
+                            Model model) {
+
+        if (loginUser == null) {
+            return "home";
+        }
+
+        model.addAttribute("user", loginUser);
+        return "loginHome";
+    }
+}
--- src/main/java/com/market/carrot/controller/UserController.java
+++ src/main/java/com/market/carrot/controller/UserController.java
@@ -4,14 +4,16 @@ import com.market.carrot.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
-    @GetMapping("/")
-    public String signin(){
+    @GetMapping("/signin")
+    public String signin() {
 
+        return "/users/singin";
     }
 }
