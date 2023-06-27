fa8035f23fd1e88601b6445a2b62bf93d0891cd7
gundorit <packdev937@gmail.com>
2023-06-27 22:02:51 +0900

    feat [all]: Reset

--- src/main/java/com/market/carrot/dto/LoginRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.market.carrot.dto;
-
-import lombok.Getter;
-
-@Getter
-public class LoginRequestDto {
-    private String username;
-    private String password;
-}
--- src/main/java/com/market/carrot/entity/SessionConst.java
+++
@@ -1,5 +0,0 @@
-package com.market.carrot.entity;
-
-public class SessionConst {
-    public static final String LOGIN_USER = "loginUser";
-}
