60d6e1462b9189c9660f7f36634074fd24b5f269
gundorit <packdev937@gmail.com>
2023-06-28 19:43:02 +0900

    feat 홈 관련 업데이트
    /home 화면이 default 페이지로 나타나도록 설정

--- src/main/java/com/market/carrot/controller/HomeController.java
+++ src/main/java/com/market/carrot/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HomeController {
 
-    @GetMapping
+    @GetMapping({""," "})
     public String home() {
         return "home";
     }
